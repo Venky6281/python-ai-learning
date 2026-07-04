6b4ed4a995b2ecbece78c29e5f97c97966e29b79
venkatesh <venkateswarlumh231@gmail.com>
2026-07-04 23:11:33 +0530

    Day 1

---
+++ untitled.py
@@ -0,0 +1,29 @@
+# Small Python program showing variables and data types
+
+# Integer
+age = 25
+print("Age:", age, "Type:", type(age))
+
+# Float
+height = 5.9
+print("Height:", height, "Type:", type(height))
+
+# String
+name = "Kuruva"
+print("Name:", name, "Type:", type(name))
+
+# Boolean
+is_student = True
+print("Is Student:", is_student, "Type:", type(is_student))
+
+# List
+marks = [85, 90, 78]
+print("Marks:", marks, "Type:", type(marks))
+
+# Tuple
+coordinates = (10, 20)
+print("Coordinates:", coordinates, "Type:", type(coordinates))
+
+# Dictionary
+person = {"name": "Kuruva", "age": 25, "student": True}
+print("Person:", person, "Type:", type(person))
